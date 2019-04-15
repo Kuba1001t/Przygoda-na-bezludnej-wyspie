e95547f177ddb270e4496bc4cb7b31209667c426
Kuba1001t <49612450+Kuba1001t@users.noreply.github.com>
2019-04-15 15:34:27 +0200

    Aktualizacja 0.0.1a v
    Dodano historię (tylko w konsoli 'prawy przycisk myszy -> zbadaj -> console')

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function opcje()
 }
 function start()
 {
+    var tekst_do_napisania = ['W twoim domu:', '-Chodźcie wnuki, opowiem wam historię.', 'Podczas lotu samolotem:', '- Hej synu, popatrz przez okno, za niedługo będziemy przelatywać nad jakąś wyspą - powiedziałeś.', 'Uwaga, silnik się uszkodził, proszę zastosować procedurę zabezpieczenia - powiedział pilot.', '- O boże! - krzyczysz', 'Na wyspie:', 'Mineło około 5 godzin.', '- Co się stało - zapytałeś się samego siebie, ale teraz sam sobie wszystko przypominasz - co się stało z moim synem?!', 'Spotykasz trzech żywych ludzi i oni dołączają do ciebie.']
+    var tekst_wyswietlany;
+    console.log(tekst_do_napisania);
     var pojemnik = document.querySelector('#pojemnik');
 }
 function menuglowne()
